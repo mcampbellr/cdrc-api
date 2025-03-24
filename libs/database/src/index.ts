623488eb75c4a67f63575a360e285d404523a597
Mario Campbell R <mariocampbellr@gmail.com>
2025-03-23 19:34:13 -0600

    chore: initial commit

---
+++ libs/database/src/index.ts
@@ -0,0 +1,11 @@
+import { PrismaAuthRepository } from './repositories/auth.repository';
+/* import { PrismaRefreshTokenRepository } from './repositories/refresh-token.repository'; */
+/* import { PrismaUserRepository } from './repositories/users.repository'; */
+
+export const repositories = [
+  /* PrismaUserRepository, */
+  PrismaAuthRepository,
+  /* PrismaRefreshTokenRepository, */
+];
+
+
